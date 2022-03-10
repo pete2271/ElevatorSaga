96f79e95c0e2344e05bb7fe99908350f6268ea13
pete2271 <pete2271@gmail.com>
2022-03-09 19:50:27 -0600

    Update Elevator.js
    Challenge 02 Solution

--- Elevator.js
+++ Elevator.js
@@ -8,6 +8,8 @@
             elevator.goToFloor(0);
             elevator.goToFloor(1);
             elevator.goToFloor(2);
+            elevator.goToFloor(3);
+            elevator.goToFloor(4);
         });
     },
     update: function(dt, elevators, floors) {
