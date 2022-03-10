52c7ff32e3357d8b3a2bc5a2815b20226af84b69
pete2271 <pete2271@gmail.com>
2022-03-09 21:32:47 -0600

    Update Elevator.js
    More organized setup of elevator and floor behaviors.  Creation of floor request histogram and use to determine elevator path planning.

--- Elevator.js
+++ Elevator.js
@@ -1,19 +1,54 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+        let FLOOR_COUNT = floors.length;
+        let LOAD_FACTOR_THRESH = 0.5;
+        let direction = 1;
+        let requestHistogram = new Array(FLOOR_COUNT).fill(0);
+        
+        function SetPressedBehaviors(floor){
+            floor.on("up_button_pressed", function() {
+                requestHistogram[floor.floorNum()]++;
+            });
+            
+            floor.on("down_button_pressed", function() {
+                requestHistogram[floor.floorNum()]++;
+            });
+        }
+        
+        function SetIdleBehavior(elevator) {
+            elevator.on("idle", function() {
+                
+                if (elevator.loadFactor() > LOAD_FACTOR_THRESH) {
+                    let pressedFloor = elevator.getPressedFloors()[0];
+                    requestHistogram[pressedFloor] = 0;
+                    elevator.goToFloor(pressedFloor);
+                }
+                else {
+                    // Base behavior if no other checks hit.
+                    let nextFloor = requestHistogram.indexOf(Math.max())
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors
-            // This still worked for challenge 3.
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
-            elevator.goToFloor(3);
-            elevator.goToFloor(4);
-        });
+                    var max = 0;
+                    var maxIndex = 0;
+                    
+                    for (var i = 0; i < requestHistogram.length; i++){
+                        if (requestHistogram[i] > max) {
+                            maxIndex = i;
+                            max = requestHistogram[i];
+                        }
+                    }
+                    
+                    requestHistogram[maxIndex] = 0;
+                    elevator.goToFloor(maxIndex);    
+                }
+                
+            });
+        }
+        
+        floors.map(SetPressedBehaviors);
+        elevators.map(SetIdleBehavior);
     },
+        
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
-    }
+    },
 }
